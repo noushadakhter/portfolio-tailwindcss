9a97b7c5f7097dd9e53833eacde9a9b64b0a73d9
noushadakhter <163678090+noushadakhter@users.noreply.github.com>
2024-12-06 20:24:12 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-16 py-6 text-center bg-accent">
+     Copyright © 2024 Made by Noushad Akhter | All Rights Reserved.
+    </div>
+  );
+};
+
+export default Footer;
